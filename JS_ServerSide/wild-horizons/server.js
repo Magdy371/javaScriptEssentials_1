29a5d84b4224fcf19f2c9e6059c479bdc6b6e48a
Magdy371 <magdyelshrief37@gmail.com>
2025-08-27 12:44:22 +0300

    world horizon ph1 ph1

--- JS_ServerSide/wild-horizons/server.js
+++ JS_ServerSide/wild-horizons/server.js
@@ -1,19 +1,37 @@
-import http from 'node:http';
-import { getDataFromDB } from './db.js';
+import http from 'node:http'
+import { getDataFromDB } from './db.js'
+import {sendJSONResponse} from "./utils/senJSONResponse.js";
 
-const server = http.createServer(
-    async(req,res)=>{
-        const destination =await getDataFromDB();
-        if(req.url ==='/api' && req.method ==='GET'){
-            res.setHeader('Content-Type','application/json');
-            res.statusCode  = 200;
-            res.statusMessage = 'ok';
-            res.end(JSON.stringify(destination));
-        }
+const PORT = 8000
+
+const server = http.createServer(async (req, res) => {
+    const destinations = await getDataFromDB()
+
+    if (req.url === '/api' && req.method === 'GET') {
+        sendJSONResponse(res , 200,destinations);
+
+    } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+
+        const continent = req.url.split('/').pop()
+        const filteredData = destinations.filter((destination) => {
+            return destination.continent.toLowerCase() === continent.toLowerCase()
+        })
+        sendJSONResponse(res , 200,filteredData);
+
+    }else if(req.url.startsWith('/api/country')&& req.method === 'GET'){
+        const country = req.url.split(('/')).pop();
+        const filtered_country = destinations.filter((destinations)=>{
+            return destinations.country.toLowerCase()===filtered_country.toLowerCase();
+        })
+        sendJSONResponse(res , 200,filtered_country);
     }
-);
+    else {
+        sendJSONResponse(res , 404,destinations);
+        res.setHeader('Content-Type', 'application/json',{
+            error: "not found",
+            message: "The requested route does not exist"});
+    }
 
-const PORT = 5000||8000;
-server.listen(PORT,()=>{
-    console.log('server is running');
 })
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))

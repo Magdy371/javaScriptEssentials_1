8065836489661f5b2a052aa4086747c9660173f6
Magdy371 <magdyelshrief37@gmail.com>
2025-08-28 22:49:17 +0300

    inteface phase2 Methods

--- TypeScript_Essentials/src/interfaces.ts
+++ TypeScript_Essentials/src/interfaces.ts
@@ -1,13 +1,24 @@
 interface User{
     id:number;
     name:string,
-    country:string
+    country:string,
+    getName():string,
+    printMessage:()=>string,
+    getMessage(message:string):string
 }
 
+const message:string ="Heelo here" 
 let user:User ={
     id:100,
     name:"Magdy",
-    country:"Egypt"
+    country:"Egypt",
+    getName() {
+        return this.name;
+    },
+    printMessage:()=>"Magdy Elshrief",
+    getMessage(message){
+        return message;
+    }
 } 
 console.log(`user name: ${user.name}`);
 console.log(`user id: ${user.id}`);
